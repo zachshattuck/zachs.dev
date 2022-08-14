0803b7c3d4f4adc5372eced7110925da9c8de17c
Zach Shattuck <zdogshattuck@gmail.com>
2022-08-14 15:15:50 -0600

    Firebase set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { app } from 'firebaseConfig'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
